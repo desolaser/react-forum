f63b413f0808b4299a288ade6a22c315515c82ba
Felipe Olavarria <178.felipe@gmail.com>
2020-05-22 15:29:02 -0400

    Add page components to App component into the links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Layout from 'component/Layout'
 import {BroswerRouter as Router, Link, Switch} from 'react-router-dom'
-import './App.css'
+import MainPage from 'component/MainPage'
+import PostsPage from 'component/PostsPage'
+import SinglePostPage from 'component/SinglePostPage'
 
 function App() {
   return (
     <Layout>
       <Router>
         <Switch>
-          <Link to="/" component={}/>
-          <Link to="/posts" component={}/>
-          <Link to="/single-post" component={}/>
+          <Link to="/" component={MainPage}/>
+          <Link to="/posts" component={PostsPage}/>
+          <Link to="/single-post" component={SinglePostPage}/>
         </Switch>
       </Router>
     </Layout>
