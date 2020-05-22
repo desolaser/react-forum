dbe573f41613e02c22534d20dc787d3e59499bbc
Felipe Olavarria <178.felipe@gmail.com>
2020-05-22 16:06:43 -0400

    Add header styles

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import {
   BrowserRouter as Router,
   Switch,
   Route
-} from "react-router-dom";
+} from "react-router-dom"
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
 import MainPage from './component/MainPage'
 import PostsPage from './component/PostsPage'
 import SinglePostPage from './component/SinglePostPage'
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Arial',
+      'sans-serif'
+    ].join(',')
+  }
+})
+
 function App() {
   return (
-    <Layout>
-      <Router>
-        <Switch>
-          <Route to="/" component={MainPage}/>
-          <Route to="/posts" component={PostsPage}/>
-          <Route to="/single-post" component={SinglePostPage}/>
-        </Switch>
-      </Router>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route to="/" component={MainPage}/>
+            <Route to="/posts" component={PostsPage}/>
+            <Route to="/single-post" component={SinglePostPage}/>
+          </Switch>
+        </Router>
+      </Layout>
+    </ThemeProvider>
   )
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    root: {
+        backgroundImage: "linear-gradient(to bottom, #FFFFFF, #C3CFE2)",
+    },
+    title: {
+        padding: "40px 20px",
+        display: "inline-block"
+    },
+    logo: {
+        float: "right",
+        maxHeight: "100px"
+    }
+})
 
 const Header = () => {
+    const classes = useStyles()
     return (
-        <header className="header">
-            <Typography variant="h1">
+        <header className={classes.root}>
+            <Typography variant="h2" align="left" className={classes.title}>
                 Forum
             </Typography>
+            <div className={classes.logo}>
+                <img src={process.env.PUBLIC_URL + '/logo.png'} alt="logo"/>
+            </div>
         </header>
     )
 }
