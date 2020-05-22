9bf20036c61ada0b873541fe94b8b0bc3331d2b8
Felipe Olavarria <178.felipe@gmail.com>
2020-05-22 16:15:40 -0400

    Add background to Body component

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
 import SideBar from './SideBar'
 
+const useStyles = makeStyles({
+    root: {
+        background: "#F2F2F2"
+    }
+})
+
 const Body = props => {
+    const classes = useStyles()
     return (
-        <div className="body">
+        <div className={classes.root}>
             <div>
                 {props.children}
             </div>
