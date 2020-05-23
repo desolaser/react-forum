2ecce078b062ebf260702d44e51c38996b2c78d2
Felipe Olavarria <178.felipe@gmail.com>
2020-05-23 11:24:18 -0400

    Fix styles for footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,35 +3,32 @@ import { makeStyles } from '@material-ui/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
 import Divider from '@material-ui/core/Divider'
-import FooterNavList from './FooterNavList'
-import DraftsIcon from "@material-ui/icons/Drafts"
-import SendIcon from "@material-ui/icons/Send"
-import StarBorder from "@material-ui/icons/StarBorder"
+import MemoryIcon from "@material-ui/icons/Memory"
+import { Avatar } from '@material-ui/core'
 
 const useStyles = makeStyles({
     root: {
-        background: "#E1E1E1",        
+        background: "#1D3557",
+        color: "white",
         padding: 20
     },
-})
-
-const NavItems = [
-    {
-      icon: <StarBorder />,
-      name: "Starred"
+    icons: {
+        background: "#E63946"
     },
-    {
-      icon: <SendIcon />,
-      name: "Mail"
+    logo: {
+        display: "inline-block",
+        float: "right"
     },
-    {
-      icon: <DraftsIcon />,
-      name: "Drafts"
+    footerTitle: {
+        display: "inline-block",
+        float: "right",
+        padding: 20
+    },
+    footerIcons: {
+        padding: 40
     }
-];
+})
 
 const Footer = () => {
     const classes = useStyles()
@@ -72,20 +69,38 @@ const Footer = () => {
                     </Typography>
                 </Grid>
                 <Grid item sm={6}>
-                    <Typography variant="h4" align="right">
+                    <img src={process.env.PUBLIC_URL + '/logo_sm.png'} alt="logo" className={classes.logo}/>
+                    <Typography variant="h4" align="right" className={classes.footerTitle}>
                         Forum
                     </Typography>
-                    <FooterNavList navItems={NavItems} />
+                    <Grid container justify="flex-end" className={classes.footerIcons}>
+                        <Grid item sm={1}>
+                            <Avatar className={classes.icons}>
+                                <MemoryIcon/>
+                            </Avatar>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Avatar className={classes.icons}>
+                                <MemoryIcon/>
+                            </Avatar>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Avatar className={classes.icons}>
+                                <MemoryIcon/>
+                            </Avatar>
+                        </Grid>
+                        <Grid item sm={1}>
+                            <Avatar className={classes.icons}>
+                                <MemoryIcon/>
+                            </Avatar>
+                        </Grid>
+                    </Grid>
                 </Grid>
-            </Grid>            
+            </Grid>
             <Divider/>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="p" align="center">
-                        Copyright @Discretuum 2020
-                    </Typography>
-                </Toolbar>
-            </AppBar>
+            <Typography variant="p" align="center">
+                Copyright @Discretuum 2020
+            </Typography>
         </Paper>
     )
 }
