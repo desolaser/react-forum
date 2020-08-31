0d54f319b61545b3cc7ca6134ba34fe6191ddadc
Felipe Olavarria <178.felipe@gmail.com>
2020-08-31 12:38:58 -0400

    Change component folder to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./component/Layout";
+import Layout from "./components/Layout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
