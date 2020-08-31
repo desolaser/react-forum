6a91b245fb4f1489ea6e93b02c922edccb8d8737
Felipe Olavarria <178.felipe@gmail.com>
2020-08-31 12:28:27 -0400

    Move views components to views folder

---
+++ src/views/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Category from "../component/Category";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <Category />
+      <Category />
+      <Category />
+    </div>
+  );
+};
+
+export default MainPage;
