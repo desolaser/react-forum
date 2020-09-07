e84909b735324c48153c9cf1b408dd9e83c3bdef
Felipe Olavarria <178.felipe@gmail.com>
2020-09-07 19:25:16 -0300

    Add redux-persist, redux-thunk y redux-logger

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import FirebaseProvider from "./modules/firebase";
 
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <FirebaseProvider>
-        <App />
-      </FirebaseProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <FirebaseProvider>
+          <App />
+        </FirebaseProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
