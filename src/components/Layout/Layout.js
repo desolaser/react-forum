99d3433002012bce6313c6e3131d8cffabf135e8
Felipe Olavarria <178.felipe@gmail.com>
2020-09-08 19:23:13 -0300

    Change layout width to 90%

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 import Header from "./Header";
 import NavBar from "./NavBar";
 import Body from "./Body";
 import Footer from "./Footer";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "#F2F2F2",
+  },
+  content: {
+    width: "90%",
+    margin: "auto",
+  },
+}));
+
 const Layout = (props) => {
+  const classes = useStyles();
   return (
-    <div className="layout">
-      <Header />
-      <NavBar />
-      <Body>{props.children}</Body>
+    <div className={classes.root}>
+      <div className={classes.content}>
+        <Header />
+        <NavBar />
+        <Body>{props.children}</Body>
+      </div>
       <Footer />
     </div>
   );
