b94b9a8c46bb1db9ca4480b0ec0bf5e9d8f4db14
Felipe Olavarria <178.felipe@gmail.com>
2020-09-30 19:20:09 -0300

    Create LoginForm component

---
+++ src/components/Layout/LoginForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+import Alert from "../Alert";
+import FormInput from "../FormInput";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: "40%",
+    backgroundColor: "#1D3557",
+    color: "white",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    textAlign: "center",
+    top: "50%",
+    left: "50%",
+    transform: `translate(-50%, -50%)`,
+  },
+  button: {
+    background: "#E63946",
+    color: "white",
+    marginTop: "20px",
+    "&:hover": {
+      background: "#A52932",
+    },
+  },
+}));
+
+const LoginModal = ({ data, error, isInvalid, handleChange, handleSubmit }) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.paper}>
+      <Typography variant="h4">Login</Typography>
+      <FormInput
+        label="Email"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+      />
+      <FormInput
+        label="Password"
+        name="password"
+        type="password"
+        value={data.password}
+        onChange={handleChange}
+      />
+      <Button
+        onClick={handleSubmit}
+        className={classes.button}
+        disabled={isInvalid}
+      >
+        Login
+      </Button>
+      {error && <Alert>{error.message}</Alert>}
+    </Paper>
+  );
+};
+
+export default LoginModal;
