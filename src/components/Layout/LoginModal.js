e17ac304a28f1d177f07010c447b1c2b764a6cf9
Felipe Olavarria <178.felipe@gmail.com>
2020-09-30 19:20:32 -0300

    Replace LoginModal form with LoginForm component

--- src/components/Layout/LoginModal.js
+++ src/components/Layout/LoginModal.js
@@ -1,51 +1,27 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/styles";
 import { useDispatch } from "react-redux";
-
 import { login } from "../../redux/actions";
 import { useFirebase } from "../../modules/firebase";
-import Alert from "../Alert";
-import FormInput from "../FormInput";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: "40%",
-    backgroundColor: "#1D3557",
-    color: "white",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    textAlign: "center",
-    top: "50%",
-    left: "50%",
-    transform: `translate(-50%, -50%)`,
-  },
-  button: {
-    background: "#E63946",
-    color: "white",
-    marginTop: "20px",
-    "&:hover": {
-      background: "#A52932",
-    },
-  },
-}));
+import LoginForm from "./LoginForm";
 
 const LoginModal = ({ closeModal }) => {
-  const classes = useStyles();
   const firebase = useFirebase();
   const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
   const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = () => {
     firebase
-      .doSignInWithEmailAndPassword(email, password)
+      .doSignInWithEmailAndPassword(data.email, data.password)
       .then((authUser) => {
         const id = authUser.user.uid;
         firebase
@@ -70,31 +46,16 @@ const LoginModal = ({ closeModal }) => {
       .catch((error) => setError(error));
   };
 
-  const isInvalid = email === "" || password === "";
+  const isInvalid = data.email === "" || data.password === "";
 
   return (
-    <Paper className={classes.paper}>
-      <Typography variant="h4">Login</Typography>
-      <FormInput
-        label="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <FormInput
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        onClick={handleSubmit}
-        className={classes.button}
-        disabled={isInvalid}
-      >
-        Login
-      </Button>
-      {error && <Alert>{error.message}</Alert>}
-    </Paper>
+    <LoginForm
+      data={data}
+      error={error}
+      isInvalid={isInvalid}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
   );
 };
 
