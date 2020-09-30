b709584642503ee87b2345df8d9e29edf0341971
Felipe Olavarria <178.felipe@gmail.com>
2020-09-30 19:13:30 -0300

    Replace form in RegisterModal with RegisterForm component

--- src/components/Layout/RegisterModal.js
+++ src/components/Layout/RegisterModal.js
@@ -1,59 +1,36 @@
 import React, { useState } from "react";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/styles";
-
 import { useFirebase } from "../../modules/firebase";
-import FormInput from "../FormInput";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    position: "absolute",
-    width: "40%",
-    backgroundColor: "#1D3557",
-    color: "white",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    textAlign: "center",
-    top: "50%",
-    left: "50%",
-    transform: `translate(-50%, -50%)`,
-  },
-  button: {
-    background: "#E63946",
-    color: "white",
-    marginTop: "20px",
-    "&:hover": {
-      background: "#A52932",
-    },
-  },
-}));
+import RegisterForm from "./RegisterForm";
 
 const RegisterModal = ({ closeModal }) => {
-  const classes = useStyles();
   const firebase = useFirebase();
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
-  const [country, setCountry] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
   const [error, setError] = useState("");
+  const [data, setData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    age: "",
+    country: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = () => {
     firebase
-      .doCreateUserWithEmailAndPassword(email, password)
+      .doCreateUserWithEmailAndPassword(data.email, data.password)
       .then((authUser) => {
         firebase.user(authUser.user.uid).set({
-          username,
-          firstName,
-          lastName,
-          email,
-          age,
-          country,
+          username: data.username,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email: data.email,
+          age: data.age,
+          country: data.country,
         });
         alert("You have been registered successfully, try to log in");
         closeModal();
@@ -62,80 +39,23 @@ const RegisterModal = ({ closeModal }) => {
   };
 
   const isInvalid =
-    username === "" ||
-    firstName === "" ||
-    lastName === "" ||
-    email === "" ||
-    age === "" ||
-    country === "" ||
-    password === "" ||
-    password !== repeatPassword;
+    data.username === "" ||
+    data.firstName === "" ||
+    data.lastName === "" ||
+    data.email === "" ||
+    data.age === "" ||
+    data.country === "" ||
+    data.password === "" ||
+    data.password !== data.repeatPassword;
 
   return (
-    <Paper className={classes.paper}>
-      <Typography variant="h4">Register</Typography>
-      <FormInput
-        label="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <FormInput
-        label="First name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-      <FormInput
-        label="Last name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
-      <FormInput
-        label="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <FormInput
-        label="Age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <FormInput
-        label="Country"
-        value={country}
-        onChange={(e) => setCountry(e.target.value)}
-      />
-      <FormInput
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <FormInput
-        label="Repeat password"
-        type="password"
-        value={repeatPassword}
-        onChange={(e) => setRepeatPassword(e.target.value)}
-      />
-      <Button
-        className={classes.button}
-        onClick={handleSubmit}
-        disabled={isInvalid}
-      >
-        Register
-      </Button>
-      {error && (
-        <Paper
-          style={{
-            backgroundColor: "tomato",
-            padding: 10,
-            marginTop: 10,
-            color: "white",
-          }}
-        >
-          {error.message}
-        </Paper>
-      )}
-    </Paper>
+    <RegisterForm
+      data={data}
+      error={error}
+      isInvalid={isInvalid}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
   );
 };
 
