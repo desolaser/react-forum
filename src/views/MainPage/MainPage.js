135393244dbe0bddbb5ced9ba3826dbdeeaaaf76
Felipe Olavarria <178.felipe@gmail.com>
2020-10-01 15:01:45 -0300

    Conditional TitleBar rendering in MainPage

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Category from "./Category";
-import TitleBar from "./TitleBar";
+import TitleBar from "../../components/TitleBar";
+import { useSelector } from "react-redux";
 
 const MainPage = () => {
+  const auth = useSelector((store) => store.auth);
+  const authExists =
+    Object.keys(auth).length !== 0 && auth.constructor === Object;
+
+  const onButtonClick = () => {
+    console.log("asd");
+  };
+
   return (
     <Container>
-      <TitleBar />
+      {authExists && (
+        <TitleBar
+          title="Category"
+          buttonText="Add category"
+          onButtonClick={onButtonClick}
+        />
+      )}
       <Category />
       <Category />
       <Category />
