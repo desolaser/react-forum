1a93c43e07bef156e409b0820aff10bf4981206a
Felipe Olavarria <178.felipe@gmail.com>
2020-10-01 16:05:20 -0300

    Get category data in MainPage

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Category from "./Category";
 import TitleBar from "../../components/TitleBar";
+import { useFirebase } from "../../modules/firebase";
 import { useSelector } from "react-redux";
 
 const MainPage = ({ history }) => {
+  const firebase = useFirebase();
+  const [categories, setCategories] = useState([]);
   const auth = useSelector((store) => store.auth);
   const authExists =
     Object.keys(auth).length !== 0 && auth.constructor === Object;
@@ -12,6 +15,25 @@ const MainPage = ({ history }) => {
   const onButtonClick = () => {
     history.push("/add-category");
   };
+
+  useEffect(() => {
+    firebase
+      .categories()
+      .once("value")
+      .then((snapshot) => {
+        snapshot.forEach((item) => {
+          const value = item.val();
+          setCategories((prevCategories) => [
+            ...prevCategories,
+            {
+              id: item.key,
+              name: value.name,
+              description: value.description,
+            },
+          ]);
+        });
+      });
+  }, []);
 
   return (
     <Container>
@@ -22,9 +44,9 @@ const MainPage = ({ history }) => {
           onButtonClick={onButtonClick}
         />
       )}
-      <Category />
-      <Category />
-      <Category />
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
     </Container>
   );
 };
