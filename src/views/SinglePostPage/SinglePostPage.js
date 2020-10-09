88e3deb5825a58dd3b58b963d8477b0df92288bf
Felipe Olavarria <178.felipe@gmail.com>
2020-10-09 13:40:29 -0300

    Add SinglePostPage firebase data on SinglePostPage

--- src/views/SinglePostPage/SinglePostPage.js
+++ src/views/SinglePostPage/SinglePostPage.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
+import { useFirebase } from "../../modules/firebase";
 
 import UserProfile from "./UserProfile";
 import Content from "./Content";
+import CardNote from "../../components/CardNote";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,36 +14,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const user = {
-  name: "John Doe",
-  role: "Admin",
-  posts: [{}, {}, {}],
-  comments: [{}, {}, {}],
-  createdAt: "06-09-1990",
-};
-
-const post = {
-  date: "Tuesday at 2:40",
-  number: 1,
-  text: "Lorem Ipsum is simply dummy text of the printing and typesetting",
-};
-
-const SinglePostPage = () => {
+const SinglePostPage = ({ match }) => {
   const classes = useStyles();
+  const firebase = useFirebase();
+  const [user, setUser] = useState({});
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState({});
+
+  useEffect(() => {
+    firebase.postWithComments(match.params.id, async (postData) => {
+      const toWait = [];
+      postData.comments &&
+        postData.comments.forEach((comment) => {
+          const method = firebase.commentWithUser(comment, (commentData) => {
+            comment = commentData;
+          });
+          toWait.push(method);
+        });
+      await Promise.all(toWait);
+      setPost(postData);
+      firebase
+        .user(postData.user)
+        .once("value")
+        .then((snapshot) => {
+          setUser(snapshot.val());
+          setLoading(false);
+        });
+    });
+  }, []);
+
   return (
     <div className={classes.root}>
-      <Grid container spacing={2}>
-        <UserProfile user={user} />
-        <Content post={post} />
-      </Grid>
-      <Grid container spacing={2}>
-        <UserProfile user={user} />
-        <Content post={post} />
-      </Grid>
-      <Grid container spacing={2}>
-        <UserProfile user={user} />
-        <Content post={post} />
-      </Grid>
+      {loading ? (
+        <CardNote
+          title="Loading"
+          text="Data is loading, please wait a second"
+        />
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            <UserProfile user={user} />
+            <Content post={post} />
+          </Grid>
+          {post.comments &&
+            post.comments.map((comment) => (
+              <Grid container spacing={2}>
+                <UserProfile user={comment.user} />
+                <Content post={comment} />
+              </Grid>
+            ))}
+        </>
+      )}
     </div>
   );
 };
