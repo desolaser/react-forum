e4d42fd145e06b96a4d37bd117dc22ce8d533c29
Felipe Olavarria <178.felipe@gmail.com>
2020-10-16 17:55:58 -0300

    Add TitleBar in SinglePostPage

--- src/views/SinglePostPage/SinglePostPage.js
+++ src/views/SinglePostPage/SinglePostPage.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+
 import { useFirebase } from "../../modules/firebase";
 
 import UserProfile from "./UserProfile";
 import Content from "./Content";
 import CardNote from "../../components/CardNote";
+import TitleBar from "../../components/TitleBar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,12 +18,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const SinglePostPage = ({ match }) => {
+const SinglePostPage = ({ match, history }) => {
   const classes = useStyles();
   const firebase = useFirebase();
   const [user, setUser] = useState({});
   const [post, setPost] = useState({});
   const [loading, setLoading] = useState({});
+  const auth = useSelector((store) => store.auth);
+  const authExists =
+    Object.keys(auth).length !== 0 && auth.constructor === Object;
 
   useEffect(() => {
     firebase.postWithComments(match.params.id, async (postData) => {
@@ -43,8 +50,17 @@ const SinglePostPage = ({ match }) => {
     });
   }, []);
 
+  const addComment = () => {
+    history.push("/add-comment/" + post.id);
+  };
+
   return (
-    <div className={classes.root}>
+    <Container className={classes.root}>
+      <TitleBar
+        title={loading ? "Loading post" : `Posts page`}
+        buttonText="Add comment"
+        onButtonClick={authExists && addComment}
+      />
       {loading ? (
         <CardNote
           title="Loading"
@@ -52,7 +68,7 @@ const SinglePostPage = ({ match }) => {
         />
       ) : (
         <>
-          <Grid container spacing={2}>
+          <Grid style={{ marginTop: 20 }} container spacing={2}>
             <UserProfile user={user} />
             <Content post={post} />
           </Grid>
@@ -65,7 +81,7 @@ const SinglePostPage = ({ match }) => {
             ))}
         </>
       )}
-    </div>
+    </Container>
   );
 };
 
